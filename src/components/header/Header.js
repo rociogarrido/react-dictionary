18816ab97c0d28f2df80965aa047d2881b240e8c
Rocío Garrido <rocioganon@gmail.com>
2021-08-31 16:20:14 +0200

    add TextField functionality

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,14 @@
 import React from "react";
+import "./Header.css";
+import { TextField } from "@material-ui/core";
 
 const Header = () => {
   return (
     <div className="header">
       <span className="title">Word Hunt</span>
+      <div className="inputs">
+        <TextField id="standard-basic" label="Enter a word" />
+      </div>
     </div>
   );
 };
