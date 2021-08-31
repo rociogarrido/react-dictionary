c7d4e04f28bf35294817eaaa94b06690d7a2df21
Rocío Garrido <rocioganon@gmail.com>
2021-08-31 15:53:21 +0200

    add some inline styles to container

--- src/App.js
+++ src/App.js
@@ -1,17 +1,49 @@
 import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Container } from "@material-ui/core";
 
 function App() {
+  // state management
+  const [meanings, setMeanings] = useState([]);
+  const [word, setWord] = useState("");
+
+  // fetch data from API
   const dictionaryApi = async () => {
     try {
       const data = await axios.get(
         `https://api.dictionaryapi.dev/api/v2/entries/en/plane`
       );
+      // console.log(data);
+      setMeanings(data.data);
     } catch (error) {
       console.log(error);
     }
   };
 
-  return <div className="App">hello world</div>;
+  console.log(meanings);
+
+  useEffect(() => {
+    dictionaryApi();
+  }, []);
+
+  return (
+    <div
+      className="App"
+      style={{
+        backgroundColor: "#282c34",
+        color: "white",
+        height: "100vh",
+      }}
+    >
+      <Container
+        maxWidth="md"
+        style={{ display: "flex", flexDirection: "column", height: "100vh" }}
+      >
+        Dictionary
+      </Container>
+    </div>
+  );
 }
 
 export default App;
