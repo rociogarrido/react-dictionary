b631e6f788481a7af1822b269907ac6aa039dc74
Rocío Garrido <rocioganon@gmail.com>
2021-08-31 17:31:41 +0200

    add ThemeProvider Component

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,13 +1,27 @@
 import React from "react";
 import "./Header.css";
-import { TextField } from "@material-ui/core";
+import { TextField, ThemeProvider, createTheme } from "@material-ui/core";
 
 const Header = () => {
+  // Material UI Dark Theme
+  const darkTheme = createTheme({
+    palette: {
+      primary: {
+        main: "#fff",
+      },
+      type: "dark",
+    },
+  });
+
   return (
     <div className="header">
       <span className="title">Word Hunt</span>
       <div className="inputs">
-        <TextField id="standard-basic" label="Enter a word" />
+        {/* Material UI Theme Provider Component */}
+        <ThemeProvider theme={darkTheme}>
+          {/* Material UI TextField Component */}
+          <TextField id="standard-basic" label="Enter a word" />
+        </ThemeProvider>
       </div>
     </div>
   );
