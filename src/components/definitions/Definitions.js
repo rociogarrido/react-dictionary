928df0ed17b2d2cdf0fdd685f3ccd68110c01fdd
Rocío Garrido <rocioganon@gmail.com>
2021-09-01 18:27:11 +0200

    add the audio component

--- src/components/definitions/Definitions.js
+++ src/components/definitions/Definitions.js
@@ -4,6 +4,17 @@ import "./Definitions.css";
 const Definitions = ({ word, meanings, category }) => {
   return (
     <div className="meanings">
+      {/* Render audio element if it exists, the language is english and a search has been initiated */}
+      {meanings[0] && word && category === "en" && (
+        <audio
+          src={meanings[0].phonetics[0] && meanings[0].phonetics[0].audio}
+          style={{ backgroundColor: "#fff", borderRadius: 10 }}
+          controls
+        >
+          Your browser does not support the audio element
+        </audio>
+      )}
+      {/* Render different subtitles depending on if a search has been initialized or not */}
       {word === "" ? (
         <span className="subTitle">Start by typing a word in search</span>
       ) : (
